18802076defb88ed134f0539d0022d94378405c9
Iniesta <mohammad.h.b.7798@gmail.com>
2020-10-04 16:08:03 +0330

    add pagination and create models for comments

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,15 @@ class Blog(models.Model):
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
 
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Comment bu {self.name} on {self.blog}'
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 # Create your views here.
 def allblog(request):
-    blogs = Blog.objects
-    return render(request, 'blog/allblog.html', {'blogs': blogs})
+    object_list = Blog.objects.all()
+    paginator = Paginator(object_list, 1)
+    page = request.GET.get('page')
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
+    return render(request, 'blog/allblog.html', {'blogs': blogs, 'page': page})
 
 
 def detail(request, blog_id):
